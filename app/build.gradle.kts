7857017ec8bb4afeaed0c3006cad80f56e9e1b9d
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-04-06 22:56:24 +0530

    added project dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id ("dagger.hilt.android.plugin")
+    id ("com.google.devtools.ksp")
+    id("com.mikepenz.aboutlibraries.plugin" version "10.5.2")
 }
+apply plugin: ("com.mikepenz.aboutlibraries.plugin")
 
 android {
     namespace = "com.example.wellspring"
@@ -20,6 +24,9 @@ android {
         }
     }
 
+    ksp {
+        arg('room.schemaLocation', "$projectDir/schemas")
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -38,6 +45,9 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+    lintOptions {
+        abortOnError false
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -66,4 +76,52 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation ("com.google.android.material:material:1.11.0")
+    // Android 12+ splash API.
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+    // Gson JSON library.
+    implementation ("com.google.code.gson:gson:2.10.1")
+    // OkHttp library.
+    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+    // Coil Image loading library.
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+    // Room database components.
+    implementation ("androidx.room:room-ktx:$room_version")
+    ksp ("androidx.room:room-compiler:$room_version")
+    androidTestImplementation ("androidx.room:room-testing:$room_version")
+    // Dagger - Hilt.
+    implementation ("com.google.dagger:hilt-android:$hilt_version")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+    ksp ("com.google.dagger:hilt-android-compiler:$hilt_version")
+    ksp ("androidx.hilt:hilt-compiler:1.2.0")
+    // Jsoup HTML Parser.
+    implementation ("org.jsoup:jsoup:1.17.2")
+    // Lottie animations.
+    implementation ("com.airbnb.android:lottie-compose:4.1.0")
+    // DataStore Preferences
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    // Open Source Libraries Screen.
+    implementation ("com.mikepenz:aboutlibraries-core:10.5.2")
+    implementation ("com.mikepenz:aboutlibraries-compose:10.5.2")
+    // Swipe actions.
+    implementation ("me.saket.swipe:swipe:1.2.0")
+    // Crash Handler.
+    implementation ("cat.ereza:customactivityoncrash:2.4.0")
+    // Kotlin reflect API.
+    implementation ("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
+    // Testing components.
+    testImplementation ("junit:junit:4.13.2")
+    testImplementation ("com.google.truth:truth:1.1.3")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation ("org.mockito:mockito-core:5.3.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    // Android testing components.
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+    // debug components.
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,38 @@
+lateinit val gradle_version: String
+lateinit var kotlin_version: String
+lateinit var hilt_version: String
+lateinit var room_version: String
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+
+
+// Define versions at the top-level for accessibility
+ext {
+   var kotlin_version = "1.9.22"
+   var gradle_version = "8.3.1"
+   var hilt_version = "2.49"
+    var room_version = "2.6.1"
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath ("com.android.tools.build:gradle:$gradle_version")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
+        // Note: Application dependencies should be in the module build.gradle
+    }
+}
+
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id ("com.android.application" version  "$gradle_version" apply false)
+    id ("com.android.library" version "$gradle_version" apply false)
+    id ("org.jetbrains.kotlin.android" version "$kotlin_version" apply false)
+    id ("com.google.devtools.ksp" version "1.9.22-1.0.17" apply false)
+}
 }
