ba07de234a0a8753d4deafa1d761a02a0026b95b
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-04-09 22:11:46 +0530

    implemented book repository

---
+++ app/src/main/java/com/example/wellspring/repo/BookRepository.kt
@@ -0,0 +1,177 @@
+
+
+package com.starry.myne.repo
+
+import android.content.Context
+import com.google.gson.Gson
+import com.starry.myne.BuildConfig
+import com.starry.myne.repo.models.BookSet
+import com.starry.myne.repo.models.ExtraInfo
+import com.starry.myne.utils.book.BookLanguage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.Cache
+import okhttp3.CacheControl
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.File
+import java.io.IOException
+import java.net.URLEncoder
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class CacheInterceptor : Interceptor {
+
+    companion object {
+        const val CACHE_SIZE = 32L * 1024L * 1024L // 32 MiB
+        private const val CACHE_MAX_AGE = 10 // 10 days
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response: Response = chain.proceed(chain.request())
+        val cacheControl = CacheControl.Builder()
+            .maxAge(CACHE_MAX_AGE, TimeUnit.DAYS)
+            .build()
+        return response.newBuilder()
+            .header("Cache-Control", cacheControl.toString())
+            .build()
+    }
+}
+
+class BookRepository(context: Context) {
+
+    private val baseApiUrl = "https://myne.pooloftears.xyz/books"
+    private val googleBooksUrl = "https://www.googleapis.com/books/v1/volumes"
+
+    @Suppress("USELESS_ELVIS")
+    private val googleApiKey =
+        BuildConfig.GOOGLE_API_KEY ?: "AIzaSyBCaXx-U0sbEpGVPWylSggC4RaR4gCGkVE" // Backup API key
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(100, TimeUnit.SECONDS)
+        .cache(Cache(File(context.cacheDir, "http-cache"), CacheInterceptor.CACHE_SIZE))
+        .addNetworkInterceptor(CacheInterceptor())
+        .build()
+
+    private val gsonClient = Gson()
+
+
+    suspend fun getAllBooks(
+        page: Long,
+        bookLanguage: BookLanguage = BookLanguage.AllBooks
+    ): Result<BookSet> {
+        var url = "${baseApiUrl}?page=$page"
+        if (bookLanguage != BookLanguage.AllBooks) {
+            url += "&languages=${bookLanguage.isoCode}"
+        }
+        val request = Request.Builder().get().url(url).build()
+        return makeApiRequest(request)
+    }
+
+    suspend fun searchBooks(query: String): Result<BookSet> {
+        val encodedString = withContext(Dispatchers.IO) {
+            URLEncoder.encode(query, "UTF-8")
+        }
+        val request = Request.Builder().get().url("${baseApiUrl}?search=$encodedString").build()
+        return makeApiRequest(request)
+    }
+
+    suspend fun getBookById(bookId: String): Result<BookSet> {
+        val request = Request.Builder().get().url("${baseApiUrl}?ids=$bookId").build()
+        return makeApiRequest(request)
+    }
+
+    suspend fun getBooksByCategory(
+        category: String,
+        page: Long,
+        bookLanguage: BookLanguage = BookLanguage.AllBooks
+    ): Result<BookSet> {
+        var url = "${baseApiUrl}?page=$page&topic=$category"
+        if (bookLanguage != BookLanguage.AllBooks) {
+            url += "&languages=${bookLanguage.isoCode}"
+        }
+        val request = Request.Builder().get().url(url).build()
+        return makeApiRequest(request)
+    }
+
+    private suspend fun makeApiRequest(request: Request): Result<BookSet> =
+        suspendCoroutine { continuation ->
+            okHttpClient.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    continuation.resume(Result.failure(exception = e))
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+                        continuation.resume(
+                            Result.success(
+                                gsonClient.fromJson(response.body!!.string(), BookSet::class.java)
+                            )
+                        )
+                    }
+                }
+            })
+        }
+
+    suspend fun getExtraInfo(bookName: String): ExtraInfo? = suspendCoroutine { continuation ->
+        val encodedName = URLEncoder.encode(bookName, "UTF-8")
+        val url = "${googleBooksUrl}?q=$encodedName&startIndex=0&maxResults=1&key=$googleApiKey"
+        val request = Request.Builder().get().url(url).build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                continuation.resumeWithException(e)
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    continuation.resume(parseExtraInfoJson(response.body!!.string()))
+                }
+            }
+        })
+    }
+
+    fun parseExtraInfoJson(jsonString: String): ExtraInfo? {
+        return try {
+            val jsonObj = JSONObject(jsonString)
+            val totalItems = jsonObj.getInt("totalItems")
+            if (totalItems != 0) {
+                val items = jsonObj.getJSONArray("items")
+                val item = items.getJSONObject(0)
+                val volumeInfo = item.getJSONObject("volumeInfo")
+                val imageLinks = volumeInfo.getJSONObject("imageLinks")
+                // Build Extra info.
+                val coverImage = imageLinks.getString("thumbnail").replace(
+                    "http://", "https://"
+                )
+                val pageCount = try {
+                    volumeInfo.getInt("pageCount")
+                } catch (exc: JSONException) {
+                    0
+                }
+                val description = try {
+                    volumeInfo.getString("description")
+                } catch (exc: JSONException) {
+                    ""
+                }
+                ExtraInfo(coverImage, pageCount, description)
+            } else {
+                null
+            }
+        } catch (exc: JSONException) {
+            exc.printStackTrace()
+            null
+        }
+    }
+
+}
