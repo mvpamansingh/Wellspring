a007c147506718e4f22ac3908ab2f8d8b9f38c97
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-04-10 00:29:11 +0530

    implemented Author details

---
+++ app/src/main/java/com/example/wellspring/repo/models/Author.kt
@@ -0,0 +1,17 @@
+
+
+package com.starry.myne.repo.models
+
+
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
+data class Author(
+    @SerializedName("name")
+    val name: String = "N/A",
+    @SerializedName("birth_year")
+    val birthYear: Int,
+    @SerializedName("death_year")
+    val deathYear: Int
+)
