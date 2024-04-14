59b0b16a6f2ead1b1d15ad25ea7c3f2beda47a93
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-04-14 08:12:27 +0530

    implemented navgraph, topbar and navigation stuff

---
+++ app/src/main/java/com/example/wellspring/ui/common/TopAppBars.kt
@@ -0,0 +1,173 @@
+
+
+package com.starry.myne.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.starry.myne.R
+import com.starry.myne.ui.theme.pacificoFont
+
+@Composable
+fun CustomTopAppBar(headerText: String, iconRes: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 7.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = headerText,
+                fontSize = 28.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontFamily = pacificoFont
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(id = iconRes),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.size(28.dp)
+            )
+        }
+        HorizontalDivider(
+            thickness = 2.dp,
+            color = MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp)
+        )
+    }
+}
+
+@Composable
+fun CustomTopAppBar(headerText: String, onBackButtonClicked: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp, top = 16.dp, bottom = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+        ) {
+            TopBarActionItem(
+                icon = Icons.AutoMirrored.Outlined.ArrowBack,
+                onclick = onBackButtonClicked
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = headerText,
+                modifier = Modifier.padding(bottom = 16.dp),
+                color = MaterialTheme.colorScheme.onBackground,
+                fontStyle = MaterialTheme.typography.headlineMedium.fontStyle,
+                fontFamily = pacificoFont,
+                fontSize = 24.sp
+            )
+            Spacer(modifier = Modifier.weight(2.35f))
+        }
+        HorizontalDivider(
+            thickness = 2.dp,
+            color = MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp)
+        )
+    }
+}
+
+@Composable
+fun CustomTopAppBar(
+    headerText: String,
+    actionIconRes: Int,
+    onBackButtonClicked: () -> Unit,
+    onActionClicked: () -> Unit
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp, top = 16.dp, bottom = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+        ) {
+            TopBarActionItem(
+                icon = Icons.AutoMirrored.Outlined.ArrowBack,
+                onclick = onBackButtonClicked
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = headerText,
+                modifier = Modifier.padding(bottom = 16.dp),
+                color = MaterialTheme.colorScheme.onBackground,
+                fontStyle = MaterialTheme.typography.headlineMedium.fontStyle,
+                fontFamily = pacificoFont,
+                fontSize = 24.sp
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            TopBarActionItem(
+                icon = ImageVector.vectorResource(id = actionIconRes),
+                onclick = onActionClicked
+            )
+        }
+        HorizontalDivider(
+            thickness = 2.dp,
+            color = MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp)
+        )
+    }
+
+}
+
+@Composable
+private fun TopBarActionItem(icon: ImageVector, onclick: () -> Unit) {
+    Box(modifier = Modifier
+        .padding(start = 16.dp, top = 2.dp, bottom = 18.dp, end = 16.dp)
+        .clip(CircleShape)
+        .background(MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp))
+        .clickable { onclick() }) {
+        Icon(
+            imageVector = icon,
+            contentDescription = stringResource(id = R.string.back_button_desc),
+            tint = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(10.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopAppBarsPV() {
+    Column(Modifier.fillMaxSize()) {
+        CustomTopAppBar(headerText = "Something", iconRes = R.drawable.ic_nav_categories)
+        CustomTopAppBar(headerText = "Something", onBackButtonClicked = {})
+        CustomTopAppBar(
+            headerText = "Something",
+            actionIconRes = R.drawable.ic_sort_language,
+            onBackButtonClicked = { /*TODO*/ }, onActionClicked = {})
+    }
+}
---
+++ app/src/main/java/com/example/wellspring/ui/navigation/NavGraph.kt
@@ -0,0 +1,219 @@
+
+
+package com.starry.myne.ui.navigation
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.starry.myne.ui.screens.categories.composables.CategoriesScreen
+import com.starry.myne.ui.screens.categories.composables.CategoryDetailScreen
+import com.starry.myne.ui.screens.detail.composables.BookDetailScreen
+import com.starry.myne.ui.screens.home.composables.HomeScreen
+import com.starry.myne.ui.screens.library.composables.LibraryScreen
+import com.starry.myne.ui.screens.reader.composables.ReaderDetailScreen
+import com.starry.myne.ui.screens.settings.composables.AboutScreen
+import com.starry.myne.ui.screens.settings.composables.OSLScreen
+import com.starry.myne.ui.screens.settings.composables.SettingsScreen
+import com.starry.myne.ui.screens.welcome.composables.WelcomeScreen
+import com.starry.myne.utils.NetworkObserver
+
+
+private const val NAVIGATION_ANIM_DURATION = 300
+private const val FADEIN_ANIM_DURATION = 400
+
+private fun enterTransition() = slideInHorizontally(
+    initialOffsetX = { NAVIGATION_ANIM_DURATION }, animationSpec = tween(
+        durationMillis = NAVIGATION_ANIM_DURATION, easing = FastOutSlowInEasing
+    )
+) + fadeIn(animationSpec = tween(NAVIGATION_ANIM_DURATION))
+
+private fun exitTransition() = slideOutHorizontally(
+    targetOffsetX = { -NAVIGATION_ANIM_DURATION }, animationSpec = tween(
+        durationMillis = NAVIGATION_ANIM_DURATION, easing = FastOutSlowInEasing
+    )
+) + fadeOut(animationSpec = tween(NAVIGATION_ANIM_DURATION))
+
+private fun popEnterTransition() = slideInHorizontally(
+    initialOffsetX = { -NAVIGATION_ANIM_DURATION }, animationSpec = tween(
+        durationMillis = NAVIGATION_ANIM_DURATION, easing = FastOutSlowInEasing
+    )
+) + fadeIn(animationSpec = tween(NAVIGATION_ANIM_DURATION))
+
+private fun popExitTransition() = slideOutHorizontally(
+    targetOffsetX = { NAVIGATION_ANIM_DURATION }, animationSpec = tween(
+        durationMillis = NAVIGATION_ANIM_DURATION, easing = FastOutSlowInEasing
+    )
+) + fadeOut(animationSpec = tween(NAVIGATION_ANIM_DURATION))
+
+
+@Composable
+fun NavGraph(
+    startDestination: String,
+    navController: NavHostController,
+    networkStatus: NetworkObserver.Status,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = Modifier.background(MaterialTheme.colorScheme.background)
+    ) {
+
+        /** Welcome Screen */
+        composable(
+            route = Screens.WelcomeScreen.route,
+            exitTransition = { exitTransition() },
+            popEnterTransition = { popEnterTransition() },
+        ) {
+            WelcomeScreen(navController = navController)
+        }
+
+        /** Home Screen */
+        composable(route = BottomBarScreen.Home.route,
+            enterTransition = { fadeIn(animationSpec = tween(FADEIN_ANIM_DURATION)) },
+            exitTransition = {
+                if (initialState.destination.route == Screens.BookDetailScreen.route) {
+                    exitTransition()
+                } else fadeOut(animationSpec = tween(FADEIN_ANIM_DURATION))
+            },
+            popEnterTransition = {
+                if (targetState.destination.route == Screens.BookDetailScreen.route) {
+                    popEnterTransition()
+                } else fadeIn(animationSpec = tween(FADEIN_ANIM_DURATION))
+            },
+            popExitTransition = { fadeOut(animationSpec = tween(FADEIN_ANIM_DURATION)) }) {
+            HomeScreen(navController, networkStatus)
+        }
+
+        /** Book Detail Screen */
+        composable(
+            route = Screens.BookDetailScreen.route,
+            arguments = listOf(
+                navArgument(BOOK_ID_ARG_KEY) {
+                    type = NavType.StringType
+                },
+            ),
+            enterTransition = { enterTransition() },
+            exitTransition = { exitTransition() },
+            popEnterTransition = { popEnterTransition() },
+            popExitTransition = { popExitTransition() },
+        ) { backStackEntry ->
+            val bookId = backStackEntry.arguments!!.getString(BOOK_ID_ARG_KEY)!!
+            BookDetailScreen(bookId, navController)
+        }
+
+        /** Categories Screen */
+        composable(route = BottomBarScreen.Categories.route,
+            enterTransition = { fadeIn(animationSpec = tween(FADEIN_ANIM_DURATION)) },
+            exitTransition = {
+                if (initialState.destination.route == Screens.CategoryDetailScreen.route) {
+                    exitTransition()
+                } else fadeOut(animationSpec = tween(FADEIN_ANIM_DURATION))
+            },
+            popEnterTransition = {
+                if (targetState.destination.route == Screens.CategoryDetailScreen.route) {
+                    popEnterTransition()
+                } else fadeIn(animationSpec = tween(FADEIN_ANIM_DURATION))
+            },
+            popExitTransition = { fadeOut(animationSpec = tween(FADEIN_ANIM_DURATION)) }) {
+            CategoriesScreen(navController)
+        }
+
+        /** Category Detail Screen */
+        composable(
+            route = Screens.CategoryDetailScreen.route,
+            arguments = listOf(navArgument(CATEGORY_DETAIL_ARG_KEY) {
+                type = NavType.StringType
+            }),
+            enterTransition = { enterTransition() },
+            exitTransition = { exitTransition() },
+            popEnterTransition = { popEnterTransition() },
+            popExitTransition = { popExitTransition() },
+        ) { backStackEntry ->
+            val category = backStackEntry.arguments!!.getString(CATEGORY_DETAIL_ARG_KEY)!!
+            CategoryDetailScreen(category, navController, networkStatus)
+        }
+
+        /** Library Screen */
+        composable(route = BottomBarScreen.Library.route,
+            enterTransition = { fadeIn(animationSpec = tween(FADEIN_ANIM_DURATION)) },
+            exitTransition = {
+                if (initialState.destination.route == Screens.BookDetailScreen.route) {
+                    exitTransition()
+                } else fadeOut(animationSpec = tween(FADEIN_ANIM_DURATION))
+            },
+            popEnterTransition = {
+                if (targetState.destination.route == BottomBarScreen.Library.route) {
+                    popEnterTransition()
+                } else fadeIn(animationSpec = tween(FADEIN_ANIM_DURATION))
+            },
+            popExitTransition = { fadeOut(animationSpec = tween(FADEIN_ANIM_DURATION)) }) {
+            LibraryScreen(navController)
+        }
+
+        /** Reader Detail Screen */
+        composable(
+            route = Screens.ReaderDetailScreen.route,
+            arguments = listOf(navArgument(
+                BOOK_ID_ARG_KEY
+            ) {
+                type = NavType.StringType
+            }),
+            enterTransition = { enterTransition() },
+            exitTransition = { exitTransition() },
+            popEnterTransition = { popEnterTransition() },
+            popExitTransition = { popExitTransition() },
+        ) { backStackEntry ->
+            val bookId = backStackEntry.arguments!!.getString(BOOK_ID_ARG_KEY)!!
+            ReaderDetailScreen(
+                bookId = bookId, navController = navController, networkStatus = networkStatus
+            )
+        }
+
+        /** Settings Screen */
+        composable(route = BottomBarScreen.Settings.route,
+            enterTransition = { fadeIn(animationSpec = tween(FADEIN_ANIM_DURATION)) },
+            exitTransition = {
+                if (initialState.destination.route == Screens.OSLScreen.route || initialState.destination.route == Screens.AboutScreen.route) {
+                    exitTransition()
+                } else fadeOut(animationSpec = tween(FADEIN_ANIM_DURATION))
+            },
+            popEnterTransition = {
+                if (initialState.destination.route == Screens.OSLScreen.route || initialState.destination.route == Screens.AboutScreen.route) {
+                    popEnterTransition()
+                } else fadeIn(animationSpec = tween(FADEIN_ANIM_DURATION))
+            },
+            popExitTransition = { fadeOut(animationSpec = tween(FADEIN_ANIM_DURATION)) }) {
+            SettingsScreen(navController)
+        }
+
+        /** Open Source Licenses Screen */
+        composable(
+            route = Screens.OSLScreen.route,
+            enterTransition = { enterTransition() },
+            popExitTransition = { popExitTransition() },
+        ) {
+            OSLScreen(navController = navController)
+        }
+
+        /** About Screen */
+        composable(
+            route = Screens.AboutScreen.route,
+            enterTransition = { enterTransition() },
+            popExitTransition = { popExitTransition() },
+        ) {
+            AboutScreen(navController = navController)
+        }
+    }
+}
