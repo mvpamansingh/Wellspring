e7ba883dee5cb265bf94f728dff46d628334d90c
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-04-15 16:27:42 +0530

    created reader book functinality

---
+++ app/src/main/java/com/example/wellspring/ui/screens/reader/composables/ReaderContent.kt
@@ -0,0 +1,141 @@
+
+
+package com.starry.myne.ui.screens.reader.composables
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.starry.myne.epub.BookTextMapper
+import com.starry.myne.epub.models.EpubChapter
+import com.starry.myne.ui.screens.reader.viewmodels.ReaderScreenState
+import com.starry.myne.ui.screens.reader.viewmodels.ReaderViewModel
+import com.starry.myne.ui.theme.pacificoFont
+import com.starry.myne.utils.noRippleClickable
+
+
+private fun chunkText(text: String): List<String> {
+    return text.splitToSequence("\n\n")
+        .filter { it.isNotBlank() }
+        .toList()
+}
+
+@Composable
+fun ReaderContent(
+    viewModel: ReaderViewModel,
+    lazyListState: LazyListState,
+) {
+    SelectionContainer {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            state = lazyListState
+        ) {
+            items(
+                count = viewModel.state.epubBook!!.chapters.size,
+                key = { index -> viewModel.state.epubBook!!.chapters[index].hashCode() }
+            ) { index ->
+                val chapter = viewModel.state.epubBook!!.chapters[index]
+                ChapterLazyItemItem(
+                    chapter = chapter,
+                    state = viewModel.state,
+                    onClick = { viewModel.toggleReaderMenu() }
+                )
+            }
+        }
+    }
+
+
+}
+
+@Composable
+private fun ChapterLazyItemItem(
+    chapter: EpubChapter,
+    state: ReaderScreenState,
+    onClick: () -> Unit
+) {
+    val epubBook = state.epubBook
+    val paragraphs = remember { chunkText(chapter.body) }
+    val targetFontSize = (state.fontSize / 10) * 1.8f
+    val fontSize by animateFloatAsState(
+        targetValue = targetFontSize,
+        animationSpec = tween(durationMillis = 300),
+        label = "fontSize"
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .noRippleClickable { onClick() }
+    ) {
+        Text(
+            modifier = Modifier.padding(start = 12.dp, end = 4.dp, top = 10.dp),
+            text = chapter.title,
+            fontSize = 24.sp,
+            lineHeight = 32.sp,
+            fontFamily = pacificoFont,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.88f)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        paragraphs.forEach { para ->
+            val imgEntry = BookTextMapper.ImgEntry.fromXMLString(para)
+            when {
+                imgEntry == null -> {
+                    Text(
+                        text = para,
+                        fontSize = fontSize.sp,
+                        lineHeight = 1.3.em,
+                        fontFamily = state.fontFamily.fontFamily,
+                        modifier = Modifier.padding(start = 14.dp, end = 14.dp, bottom = 8.dp),
+                    )
+                }
+
+                else -> {
+                    val image = epubBook?.images?.find { it.absPath == imgEntry.path }
+                    image?.let {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data(image.image)
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(vertical = 6.dp)
+                        )
+                    }
+                }
+            }
+
+        }
+
+        HorizontalDivider(
+            thickness = 2.dp,
+            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/wellspring/ui/screens/reader/viewmodels/ReaderDetailViewModel.kt
@@ -0,0 +1,77 @@
+
+
+
+package com.starry.myne.ui.screens.reader.viewmodels
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.starry.myne.database.library.LibraryDao
+import com.starry.myne.database.reader.ReaderDao
+import com.starry.myne.database.reader.ReaderData
+import com.starry.myne.epub.EpubParser
+import com.starry.myne.epub.models.EpubBook
+import com.starry.myne.repo.BookRepository
+import com.starry.myne.utils.NetworkObserver
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+data class EbookData(
+    val coverImage: String?,
+    val title: String,
+    val authors: String,
+    val epubBook: EpubBook,
+)
+
+data class ReaderDetailScreenState(
+    val isLoading: Boolean = true,
+    val ebookData: EbookData? = null,
+    val error: String? = null,
+)
+
+@HiltViewModel
+class ReaderDetailViewModel @Inject constructor(
+    private val bookRepository: BookRepository,
+    private val libraryDao: LibraryDao,
+    private val readerDao: ReaderDao,
+    private val epubParser: EpubParser
+) : ViewModel() {
+
+    var state by mutableStateOf(ReaderDetailScreenState())
+
+    val readerData: Flow<ReaderData?>?
+        get() = _readerData
+    private var _readerData: Flow<ReaderData?>? = null
+
+    fun loadEbookData(bookId: String, networkStatus: NetworkObserver.Status) {
+        viewModelScope.launch(Dispatchers.IO) {
+            // Library item is not null as this screen is only accessible from the library.
+            val libraryItem = libraryDao.getItemById(bookId.toInt())!!
+            // Get reader data if it exists.
+            _readerData = readerDao.getReaderDataAsFlow(bookId.toInt())
+            val coverImage: String? = try {
+                if (networkStatus == NetworkObserver.Status.Available) bookRepository.getExtraInfo(
+                    libraryItem.title
+                )?.coverImage else null
+            } catch (exc: Exception) {
+                null
+            }
+            // Create EbookData object.
+            val ebookData = EbookData(
+                coverImage = coverImage,
+                title = libraryItem.title,
+                authors = libraryItem.authors,
+                epubBook = epubParser.createEpubBook(libraryItem.filePath)
+            )
+            delay(500) // Add delay to avoid flickering.
+            state = state.copy(isLoading = false, ebookData = ebookData)
+        }
+    }
+}
